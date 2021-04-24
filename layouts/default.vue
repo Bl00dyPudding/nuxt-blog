877285ac01a44c952943791861bcd9e1f40b51d5
Bloody Pudding <bloodypudding@gmail.com>
2021-04-25 01:05:53 +0300

    init

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-container>
+    <el-header>
+      <Navigation />
+    </el-header>
+    <el-main>
+      <Nuxt />
+    </el-main>
+    <el-footer>footer</el-footer>
+  </el-container>
+</template>
+
+<script>
+import Navigation from "~/components/main/Navigation"
+
+export default {
+  components: {
+    Navigation
+  }
+}
+</script>
