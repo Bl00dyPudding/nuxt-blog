71aae691ee091f99f72f391d3ab37e74d371770d
Bloody Pudding <bloodypudding@gmail.com>
2021-04-27 00:53:53 +0300

    post create

---
+++ server/keys/keys.dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // MONGO_URI: `mongodb://vladilen:q12345@ds239055.mlab.com:39055/nuxt-blog`
+  MONGO_URI: `mongodb+srv://user:5jcfVhSxlyV6@cluster0.8cajb.mongodb.net/nuxt-blog`
+}
