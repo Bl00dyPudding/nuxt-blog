1471664f40be5885bd4155276d958c481232d959
Bloody Pudding <bloodypudding@gmail.com>
2021-04-27 11:29:02 +0300

    login

--- server/app.js
+++ server/app.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const authRoutes = require('./routes/auth.routes')
 const keys = require('./keys')
 const app = express()
 
-mongoose.connect(keys.MONGO_URI)
+mongoose.connect(keys.MONGO_URI,{useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => console.log('MongoDB connected...'))
   .catch(error => console.error(error))
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+
+
+app.use('/api/auth', authRoutes)
 
 module.exports = app
